9a9fdd4fff35681e5f1a132cd3f185a640dc11ed
Agnessa Nikolayev <anikolayev90@gmail.com>
2023-03-08 15:23:08 -0800

    passes all the tests

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    return console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    return console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()){
+    return string = "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+        return string = "YES INDEED!";
+    } else if (string === "Let's have dinner together!"){
+        return string = "I would love to!";
+    }
+}
